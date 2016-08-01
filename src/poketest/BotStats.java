ebf228c488123799fd0d3c0e73f520de29ff3f16
Charles Blancheton <charles.blancheton@etu.u-bordeaux.fr>
2016-08-02 00:28:22 +0200

    add xp per hour stat

--- src/poketest/BotStats.java
+++ src/poketest/BotStats.java
@@ -4,11 +4,13 @@ public class BotStats {
 	
 	private long xpStart;
 	private long currentXp;
+	private int lvl;
 	private int pokemonTransfered 	= 0;
 	private int pokemonCatched		= 0;
 	private int pokemonEvolved		= 0;
 	private int pokestopVisited 	= 0;
-	private int lvl;
+	private int xpPerHour 			= 0;
+	private long startTime;
 	
 	private int[] requiredXP = { 0, 1000, 3000, 6000, 10000, 15000, 21000, 28000, 36000, 45000, 55000, 65000, 75000,
 			85000, 100000, 120000, 140000, 160000, 185000, 210000, 260000, 335000, 435000, 560000, 710000, 900000, 1100000,
@@ -17,11 +19,13 @@ public class BotStats {
 	public BotStats(long currentXp, int lvl){
 		this.xpStart = this.currentXp = currentXp;
 		this.lvl = lvl;
+		this.startTime = System.currentTimeMillis();
 	}
 
 	public void updateStat(int lvl, long xp){
 		this.currentXp = xp;
 		this.lvl = lvl;
+		this.xpPerHour = (int) ((this.currentXp - this.xpStart) / ((System.currentTimeMillis() - startTime) * 1.f / (1000 * 60 * 60)));
 	}
 	
 	public void showStats(CustomLogger logger){
@@ -36,6 +40,7 @@ public class BotStats {
 		logger.important("Pokemon catched: " + this.pokemonCatched);
 		logger.important("Pokemon transfered: " + this.pokemonTransfered);
 		logger.important("Pokemon evolved: " + this.pokemonEvolved);
+		logger.important("XP per hour: " + this.xpPerHour);
 		logger.important("--------------");
 	}
 	
